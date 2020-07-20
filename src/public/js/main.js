4a7788c22eaea041a2f64d72bf5669bfee0bc6e2
walkexperience <walkexpereince@gmail.com>
2020-07-20 11:20:13 +0200

    ISS add to map

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,7 +1,7 @@
 // Utlizaremos leaflet
 // Al instanciar leaflet crea la variable L
 //console.log(L);
-const map = L.map('map-template').setView([51.505, -0.09], 4);
+const map = L.map('map-template').setView([0, 0], 2);
 // .addTo() layer método q permite añadir elemento titleLayer a un elemento
 //L.tileLayer('https://a.tile.openstreetmap.de/{z}/{x}/{y}.png').addTo(map);
 
@@ -43,4 +43,34 @@ const marker = L.marker([51.5, -0.09]);
 marker.bindPopup('Hello There');
 map.addLayer(marker);
 
+//
+const myIcon = L.icon({
+    iconUrl: 'https://walkexperience.org/wp-content/uploads/2020/06/Walk-logo-97.png',//ruta no correcta !!!
+    iconSize: [25, 45],
+    iconAnchor: [12, 41],
+    popupAnchor: [-3, -76],
+});
+L.marker([50.505, 30.57], {icon: myIcon}).addTo(map);
+
 //Con el protocolo webscket el servidor http envía eventos en tiempo real cuando los usuarios se conectan
+
+// Api get coordenates estación espacial
+const url_apiISS = 'http://api.open-notify.org/iss-now.json';
+async function getISS() {
+    const res = await fetch(url_apiISS);
+    const data = await res.json();
+    console.log(data);
+    const {latitude, longitude} = data.iss_position;
+    document.getElementById('lat').textContent = latitude;
+    document.getElementById('lng').textContent = longitude;
+    // creamos marcador ISS
+    const markerISS = L.marker([latitude, longitude]);
+    markerISS.bindPopup('Hello ISS');
+    map.addLayer(markerISS);
+    setTimeout(getISS, 5000);
+}
+
+setTimeout( getISS(),2000);
+
+
+
